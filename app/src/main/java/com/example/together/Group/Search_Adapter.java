37c5f31b1b765a94b912b371d61f4db8b8399af6
fjdkslvn <dhtnsehf9@gmail.com>
2021-04-08 11:39:18 +0900

    커스텀리스트 뷰에 다이얼로그 추가!

--- app/src/main/java/com/example/together/Group/Search_Adapter.java
+++ app/src/main/java/com/example/together/Group/Search_Adapter.java
@@ -1,17 +1,28 @@
 package com.example.together.Group;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.together.Calendar.Calendar_note;
 import com.example.together.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -20,6 +31,10 @@ public class Search_Adapter extends RecyclerView.Adapter<Search_Adapter.CustomVi
 
     private ArrayList<Together_group_list> arrayList;
     private Context context;
+
+    Dialog PlanDialog;//일정 기록을 위한 Dialog
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
 
     public Search_Adapter(ArrayList<Together_group_list> arrayList, Context context) {
@@ -41,6 +56,15 @@ public class Search_Adapter extends RecyclerView.Adapter<Search_Adapter.CustomVi
         Glide.with(holder.itemView)
                 .load(arrayList.get(position).getiv_people())
                 .into(holder.iv_people); */
+
+        //다이얼로그 관련 설정
+        PlanDialog=new Dialog(context); //context로 하니까 잘 됩니다.
+        PlanDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);//제목 제거
+        PlanDialog.setContentView(R.layout.plan_dialog);
+
+        database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
+        databaseReference = database.getReference("Together_group_list");
+
         holder.Gname.setText(arrayList.get(position).getGname());
         holder.GAP.setText(String.valueOf(arrayList.get(position).getGAP()));
         holder.GCP.setText(String.valueOf(arrayList.get(position).getGCP()));
@@ -51,13 +75,11 @@ public class Search_Adapter extends RecyclerView.Adapter<Search_Adapter.CustomVi
             @Override
             public void onClick(View view) {
                 String GName = holder.Gname.getText().toString(); //그룹 이름을 저 변수에 담는다!
-
-                Intent intent = new Intent(context.getApplicationContext(), look_group.class); //그룹 상세 화면으로 연결
-                intent.putExtra("Gname", GName); //그룹 이름 넘겨서 열기
-                context.startActivity(intent); //액티비티 열기
-                Toast.makeText(view.getContext(), GName,Toast.LENGTH_SHORT).show(); //토스트로 실험
+                showPlanDialog();
             }
         });
+
+
 
 
 
@@ -83,6 +105,40 @@ public class Search_Adapter extends RecyclerView.Adapter<Search_Adapter.CustomVi
             this.GAP = itemView.findViewById(R.id.GAP);
             this.GCP = itemView.findViewById(R.id.GCP);
         }
+    }
+
+    //일정을 입력할 수 있는 다이얼로그 호출(다이얼로그 관련 코드)
+    public void showPlanDialog(){
+        PlanDialog.show(); //다이얼로그 출력
+        PlanDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));//끝부분을 둥굴게 하기 위해 투명색 지정
+        Button noBtn= PlanDialog.findViewById(R.id.noBtn);//취소 버튼
+        Button yesBtn=PlanDialog.findViewById(R.id.yesBtn);//저장 버튼
+
+        //취소 버튼
+        noBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlanDialog.dismiss();//다이얼로그 닫기
+            }
+        });
+
+        //저장 버튼
+        yesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    //databaseReference=database.getReference().child("calendar").child(checkYear + "-" + checkMonth + "-" + checkDay);
+                    //databaseReference.setValue(content);//선택한 날짜에 일정 저장
+                    //Toast.makeText(view.getContext(), GName,Toast.LENGTH_SHORT).show(); //토스트로 실험
+
+                }catch(Exception e){//예외
+                    e.printStackTrace();
+                    //Toast.makeText(getApplicationContext().getContext(),"오류발생",Toast.LENGTH_SHORT).show();//토스메세지 출력
+                }
+                PlanDialog.dismiss();//다이얼로그 닫기
+            }
+        });
+
     }
 
 }
